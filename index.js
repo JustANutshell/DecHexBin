69466e8eab01c4d82870e7ebbd217c003f8deba9
dark <54949766+JustANutshell@users.noreply.github.com>
2022-06-11 16:30:22 +0200

    v1.2.0

--- index.js
+++ index.js
@@ -1,136 +1,119 @@
 // DecHexBin by npm: Dark_ github: JustANutshell
 // https://www.npmjs.com/package/dechexbin
-module.exports=(function(){
-	var mainconfig=(function(){
-		var mainconfig={
-			nt:[
-				{n:"dec",x:10n},
-				{n:"hex",x:16n},
-				{n:"bin",x:2n},
-			],
-			c_a:{},
-			c_b:{},
-		};
-		var b="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-		for(var a=0;a<26;a++){
-			mainconfig.c_a[10+a]=b[a];
-		}
-		for(var a=0;a<10;a++){
-			mainconfig.c_a[BigInt(a)]=String(a);
-		}
-		var b=Object.keys(mainconfig.c_a);
-		for(var a=0;a<b.length;a++){
-			mainconfig.c_b[mainconfig.c_a[b[a]]]=b[a];
-		}
-		return mainconfig;
-	})();
-	var MathPow=function(c,b){
-		var out=1n;
-		for(var a=0n;a<b;a=a+1n){
-			out=out*c;
-		}
-		return out;
+const VERSION = "1.2.0";
+const DEFAULTCYPHER = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var knownNumberSystems = {
+	"dec": 10,
+	"hex": 16,
+	"bin": 2,
+};
+
+function saveMathPow(c, b, numberType) {
+	var out = numberType(1);
+	for (var a = 0; a < b; a = a + 1) {
+		out = out * c;
 	}
-	var toDec=function(value,valuetype){
-		var value=value.split('');
-		var out=0n;
-		for(var a=0;a<value.length;a++){
-			var b=value.length-1-a;
-			out=out+(BigInt(mainconfig.c_b[value[a]])*MathPow(valuetype.x,BigInt(b)));
-		}
-		return out;
+	return out;
+}
+
+function toDec(value, numberSystem, numberType, canSkipChecks) {
+
+	if (canSkipChecks && numberSystem == "0123456789")
+		return numberType(value);
+
+	var paraCypher = {};
+
+	var numberSystemSize = numberSystem.length;
+
+	for (let i = 0; i < numberSystemSize; i++) {
+		paraCypher[numberSystem[i]] = i;
 	}
-	var fromDec=function(value,outputtype){
-		var value=BigInt(value);
-		var out="";
-		var a=value;
-		while(a!==0n){
-			out=mainconfig.c_a[Number(a%outputtype.x)]+""+out;
-			a=a/outputtype.x;
-		}
-		return out;
+
+
+	var value = value.split('');
+	var out = numberType(0);
+	for (var a = 0; a < value.length; a++) {
+		var powerOfThisPosition = saveMathPow(numberType(numberSystemSize), value.length - 1 - a, numberType);
+
+		var newValue = paraCypher[value[a]];
+		if (newValue == undefined)
+			throw "bad char";
+
+		out += numberType(newValue) * powerOfThisPosition;
 	}
-	var getNT=function(name){
-		var x=null;
-		if(typeof name==="number"||typeof name==="bigint"){
-			if(!Number.isInteger(Number(name))){
-				throw "DecHexBin: number system must be a String or a INTEGER (it was: "+name+")";
-			}
-			x={n:"---",x:BigInt(name)};
-		}else{
-			var name=String(name).toLowerCase();
-			var out=null;
-			for(var a=0;a<mainconfig.nt.length&&out===null;a++){
-				if(mainconfig.nt[a].n===name){
-					out=mainconfig.nt[a];
-				}
-			}
-			x=out;
-		}
-		if(x===null){
-			return null;
-		}else{
-			var y=Object.keys(mainconfig.c_a).length;
-			if(x.x>y){
-				throw "DecHexBin: the biggest number system is "+y;
-			}else if(x.x<2){
-				throw "DecHexBin: the smallest number system is 2";
-			}else{
-				return x;
-			}
-		}
+	return out;
+}
+function fromDec(value, numberSystem, numberType) {
+	if (numberSystem == "0123456789")
+		return String(value);
+
+	var numberSystemSize = numberType(numberSystem.length);
+
+	var out = "";
+	var a = value;
+	while (a !== numberType(0)) {
+		var b = a % numberSystemSize;
+		out = numberSystem[b] + out;
+		a = (a - b) / numberSystemSize;
 	}
-	var checkValue=function(value,type){
-		if(value===""){
-			throw "DecHexBin: value was empty";
-		}else{
-			var a=value.split('');
-			var good_=[];
-			var good=true;
-			for(var b=0;b<a.length;b++){
-				if(!(mainconfig.c_b[a[b]]!==undefined&&mainconfig.c_b[a[b]]<type.x)){
-					good=false;
-					good_[good_.length]=a[b];
-				}
-			}
-			if(!good){
-				var allowed=[];
-				var a=Object.keys(mainconfig.c_a);
-				for(var b=0;b<a.length;b++){
-					if(a[b]<type.x){
-						allowed[allowed.length]=mainconfig.c_a[a[b]];
-					}
-				}
-				throw "DecHexBin: "+JSON.stringify(good_)+" isn't allowed in the value (only allowed: "+JSON.stringify(allowed)+")";
-			}
-		}
+	return out === "" ? numberSystem[0] : out;
+}
+
+function getDefaultCypherForLength(length) {
+	if (length > DEFAULTCYPHER.length) {
+		err("the biggest number system is " + DEFAULTCYPHER.length);
+	} else if (length < 2) {
+		err("the smallest number system is 2");
+	} else {
+		return DEFAULTCYPHER.slice(0, length);
 	}
-	var main=function(value,oldType,newType){
-		var value=String(value).toUpperCase();
-		var a=function(a,b,c){if(a===null){throw "DecHexBin: "+b+" ("+(typeof c)+":"+c+") isn't a number system (such as String: dec or hex or Number: 16)";}}
-		var type_old=getNT(oldType);	a(type_old,"oldType",oldType);
-		var type_new=getNT(newType);	a(type_new,"newType",newType);
-		var a=value.split('');
-		var negativ=a[0]==="-";
-		if(negativ){
-			value="";
-			for(var b=1;b<a.length;b++){
-				value=value+""+a[b];
-			}
+}
+
+function getNT(name) {
+	if (typeof name === "number" || typeof name === "bigint") {
+		name = Number(name);
+		if (!Number.isInteger(name))
+			throw "number system must be a String or a INTEGER (it was: " + name + ")";
+
+		return getDefaultCypherForLength(name);
+
+	} else if (typeof name === "string") {
+		if (name.length == 0)
+			throw "number system is an empty string";
+
+		if (name[0] == "#") { // it's a cypher
+			return name.slice(start = 1);
+		} else { // it's the name of a numer system
+			name = name.toLowerCase();
+			numerSystemLength = knownNumberSystems[name];
+
+			if (numerSystemLength == undefined || numerSystemLength == null)
+				throw "Can't find number system with name: " + name;
+
+			return getDefaultCypherForLength(numerSystemLength);
+
 		}
-		checkValue(value,type_old);
-		if(type_old.x===type_new.x){
-			return (negativ&&value!=="0"?"-":"")+value;
-		}else{
-			if(type_old.n==="dec"){
-				var newValue=fromDec(value,type_new);
-			}else if(type_new.n==="dec"){
-				var newValue=toDec(value,type_old);
-			}else{
-				var newValue=main(main(value,type_old.x,"dec"),"dec",type_new.x);
-			}
-			return (negativ&&newValue!=="0"?"-":"")+newValue;
-		}
+	} else {
+		err("number system must be a String or a Number (it was: " + typeof name + ")");
 	}
-	return main;
-})();
+}
+
+function main(value, oldType, newType, numberType = null, canSkipChecks = true) {
+	if (numberType === undefined || numberType === null) { numberType = Number; }
+	value = String(value);
+
+	oldType = getNT(oldType);
+	newType = getNT(newType);
+
+	var negativ = value[0] === "-";
+	if (negativ) {
+		value = value.substring(1);
+	}
+
+	var decValue = toDec(value, oldType, numberType, canSkipChecks);
+	var newValue = fromDec(decValue, newType, numberType, canSkipChecks);
+
+	return (negativ && newValue !== newType[0] ? "-" : "") + newValue;
+}
+module.exports = main;
+module.exports.VERSION = VERSION;
--- readme.md
+++ readme.md
@@ -3,20 +3,45 @@ Bad coded, but hey, it works.
 
 ### Syntax
 -----
+```js
+newValue = DecHexBin(oldValue, oldType, newType (, numberSystem));
+```
 
-    DecHexBin( oldValue , oldType , newType );
+`oldType` and `newType` can be a string (`"dec"`, `"hex"`, `"bin"`) or a number (`16` for hex, `10` for dec, etc).
 
-oldType and newType can be a string ("dec", "hex", "bin") or a number like 16 (16 for hex, 10 for dec, etc)
+`numberSystem` can be `Number` or `BigInt` (or something similar). By default it's `Number`.
 
 ### Example
 -----
+```js
+var DecHexBin = require("dechexbin");
 
-    const DecHexBin=require("dechexbin");
+console.log(DecHexBin("ff", "hex", "dec")); // 255
     
-    var a=DecHexBin("ff","hex","dec");
-    console.log(a); // 255
-    
-    var b=DecHexBin("1111","bin",8);
-    console.log(b); // 17
+console.log(DecHexBin("1111", "bin", 8)); // 17
+```
 
-Send bugs to darklolcrap@gmail.com
+### Other
+-----
+DecHexBin uses normal numbers by default. You can use BigInts by using another argument:
+```js
+var DecHexBin = require("dechexbin");
+
+console.log(DecHexBin("FFFFFFFFFFFFFFFF", 16, 10, BigInt));
+```
+
+If your Node version doesn't support `Array.prototype.find`:
+```js
+var DecHexBin = require("dechexbin");
+
+DecHexBin.defaultFunctions.find=function(arr,func){
+	// you can put here an alternative find function
+	// For example:
+	for(var a=0;a<arr.length;a++){
+		if(func(arr[a])){
+			return arr[a];
+		}
+	}
+	return undefined;
+}
+```
